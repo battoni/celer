cf20501b5ec1c6a67b4992f2120b18811c53a300
Guilherme Battoni <guilherme@battoni.com>
2019-11-12 18:32:56 -0300

    Updating Bulma CSS and fixing precedence on scripts.php

--- partials/components/scripts/scripts.php
+++ partials/components/scripts/scripts.php
@@ -7,15 +7,15 @@
 		<script src="./assets/js/other/handler.js"></script>
 		<script src="./assets/js/components/topnav.js"></script>
 	<!-- Global JS -->
-
-	<!-- Individual Page JS -->
-		<script src="./assets/js/pages/<?php echo $page; ?>/_<?php echo $page; ?>.js"></script>
-	<!-- Individual Page JS -->
 	
 	<!-- Specific JS *** -->
 		<?php if($page == "index") { ?>
 			<!-- Add your specific JS libs here -->
 		<?php } ?>
 	<!-- Specific JS *** -->
+
+	<!-- Individual Page JS -->
+		<script src="./assets/js/pages/<?php echo $page; ?>/_<?php echo $page; ?>.js"></script>
+	<!-- Individual Page JS -->
 	
 </body>
